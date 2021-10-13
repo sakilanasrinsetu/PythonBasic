2b76233cad6efc14f2067630ea841fdf060428b6
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-10-13 06:55:30 +0600

    print arethmatic

--- print.py
+++ print.py
@@ -4,5 +4,5 @@ num2 = 86
 print('Summation = ', num1+num2)
 print('Subtraction', num1 - num2)
 print('Multiplication', num1 * num2)
-print('Division', num1 / num2)
+print('Division', round((num1 / num2),5))
 print('Modification', num1 % num2)
