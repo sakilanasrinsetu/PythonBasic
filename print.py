3374051df52242275a7d3ca702ada51741f6bca9
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-10-13 07:03:44 +0600

    print variable type

--- print.py
+++ print.py
@@ -1,8 +1,8 @@
 # Print Mathematics
 num1 = 100
-num2 = 86
-print('Summation = ', num1+num2)
-print('Subtraction', num1 - num2)
-print('Multiplication', num1 * num2)
-print('Division', round((num1 / num2),5))
-print('Modification', num1 % num2)
+lists = {86,'a'}
+tuple_var = (86,'a')
+print('Summation = ', type(num1))
+print('Division', type(num1 /5))
+print(type(lists))
+print(type(tuple_var))
