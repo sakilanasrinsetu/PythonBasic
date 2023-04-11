a3586c56d90c9a547b28536c8195a811b34016fb
sakilanasrinsetu <sakilanasrin@gmail.com>
2023-04-11 12:40:00 +0600

    user input

---
+++ user_input.py
@@ -0,0 +1,8 @@
+name = input("Enter Your Name = ")
+age = input("Enter Your Age = ")
+cgpa = input("Enter Your CGPA = ")
+
+print('Student Information \n ...***............***...')
+print("Student Name = " + name)
+print("Student Age = " + age)
+print("Student CGPA = " + cgpa)
